a23dc5627deef89ffc638c63cc58a2b9b76d43a5
MAOZ <maooz4426@gmail.com>
2024-09-08 19:12:33 +0900

    ディレクトリ大枠セットアップ

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
