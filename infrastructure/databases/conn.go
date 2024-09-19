ecdfab6b6204cb642cd654590477b827cb870d2c
MAOZ <maooz4426@gmail.com>
2024-09-20 00:16:28 +0900

    goのdocker環境作成

--- infrastructure/databases/conn.go
+++ infrastructure/databases/conn.go
@@ -4,19 +4,32 @@ import (
 	"github.com/maooz4426/Todolist/domain/entity"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
+	"time"
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := "user:password@tcp(127.0.0.1:3306)/db?charset=utf8mb4&parseTime=True"
+	var db *gorm.DB
+	var err error
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, err
+	dsn := "user:password@tcp(todo_db:3306)/db?charset=utf8mb4&parseTime=True"
+
+	count := 5
+
+	for count > 1 {
+		if db, err = gorm.Open(mysql.Open(dsn)); err != nil {
+			time.Sleep(2 * time.Second)
+			count--
+			log.Printf("retry... count:%v\n", count)
+			continue
+		}
+		break
 	}
 
 	err = db.AutoMigrate(&entity.Todo{})
 
 	if err != nil {
+
 		return nil, err
 	}
 
